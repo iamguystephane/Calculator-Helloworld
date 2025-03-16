f505bdd708bb3dc09b9aa133532f083599bdabc8
iamguystephane <gstephane138@gmail.com>
2025-03-16 02:00:12 +0100

    added hello world text

--- hybrid-app/helloworld/App.tsx
+++ hybrid-app/helloworld/App.tsx
@@ -90,8 +90,7 @@ function App(): React.JSX.Element {
             paddingBottom: safePadding,
           }}>
           <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
+            <Text> Hello world </Text>
           </Section>
           <Section title="See Your Changes">
             <ReloadInstructions />
@@ -99,9 +98,7 @@ function App(): React.JSX.Element {
           <Section title="Debug">
             <DebugInstructions />
           </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
+          
           <LearnMoreLinks />
         </View>
       </ScrollView>
