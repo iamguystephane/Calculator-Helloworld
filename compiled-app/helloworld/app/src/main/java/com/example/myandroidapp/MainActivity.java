04c4c6eb0179d66573ad5b4980382cd7caf7d7dd
iamguystephane <gstephane138@gmail.com>
2025-03-17 15:26:34 +0100

    final commit

--- compiled-app/helloworld/app/src/main/java/com/example/myandroidapp/MainActivity.java
+++ compiled-app/helloworld/app/src/main/java/com/example/myandroidapp/MainActivity.java
@@ -1,5 +1,12 @@
 package main.java.com.example.myandroidapp;
 
-public class MainActivity {
-    
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 }
